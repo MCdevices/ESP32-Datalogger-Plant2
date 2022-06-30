c4ae16bf1fee965dde668add0244d4986b7b5949
MCdevices <matteocavalera30@gmail.com>
2022-06-30 22:57:40 +0200

    version 1.0.0

---
+++ src/main.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
+
+
+//ADC Channels
+#if CONFIG_IDF_TARGET_ESP32
+#define ADC1_EXAMPLE_CHAN0          ADC1_CHANNEL_6  //GPIO34
+#define ADC2_EXAMPLE_CHAN0          ADC2_CHANNEL_0 //GPIO4
+static const char *TAG_CH[2][10] = {{"ADC1_CH6"}, {"ADC2_CH0"}};
+#endif
+
+//ADC Attenuation
+#define ADC_EXAMPLE_ATTEN           ADC_ATTEN_DB_11
+
+//ADC Calibration
+#if CONFIG_IDF_TARGET_ESP32
+#define ADC_EXAMPLE_CALI_SCHEME     ESP_ADC_CAL_VAL_EFUSE_VREF
+#endif
+
+
+static int adc_raw[2][10];
+static int adc_raw1 = 0;
+static const char *TAG = "ADC SINGLE";
+
+//static esp_adc_cal_characteristics_t adc1_chars;
+static esp_adc_cal_characteristics_t adc2_chars;
+
+static bool adc_calibration_init(void)
+{
+    esp_err_t ret;
+    bool cali_enable = false;
+
+    ret = esp_adc_cal_check_efuse(ADC_EXAMPLE_CALI_SCHEME);
+    if (ret == ESP_ERR_NOT_SUPPORTED) {
+        ESP_LOGW(TAG, "Calibration scheme not supported, skip software calibration");
+    } else if (ret == ESP_ERR_INVALID_VERSION) {
+        ESP_LOGW(TAG, "eFuse not burnt, skip software calibration");
+    } else if (ret == ESP_OK) {
+        cali_enable = true;
+        esp_adc_cal_characterize(ADC_UNIT_2, ADC_EXAMPLE_ATTEN, ADC_WIDTH_BIT_DEFAULT, 0, &adc2_chars);
+    } else {
+        ESP_LOGE(TAG, "Invalid arg");
+    }
+
+    return cali_enable;
+}
+
+void app_main(void)
+{
+    esp_err_t ret = ESP_OK;
+    float voltage = 0.0;
+    bool cali_enable = adc_calibration_init();
+
+    //ADC2 config
+    ESP_ERROR_CHECK(adc2_config_channel_atten(ADC2_EXAMPLE_CHAN0, ADC_EXAMPLE_ATTEN));
+
+    while (1) {
+    //while (ret == ESP_ERR_INVALID_STATE);
+        ret = adc2_get_raw(ADC2_EXAMPLE_CHAN0, ADC_WIDTH_BIT_DEFAULT, &adc_raw[1][0]);
+        ESP_ERROR_CHECK(ret);
+
+        ESP_LOGI(TAG_CH[1][0], "raw  data: %d", adc_raw[1][0]);
+        if (cali_enable) {
+            voltage = esp_adc_cal_raw_to_voltage(adc_raw[1][0], &adc2_chars);
+            if(adc_raw[1][0] == 0){
+                voltage = 0.0;
+            }
+            ESP_LOGI(TAG_CH[1][0], "cali data: %f mV", voltage);
+        }
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
