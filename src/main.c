09a2a517f430481cdcefa4a9c59bba6e91341ad6
MCdevices <matteocavalera30@gmail.com>
2022-07-02 18:32:21 +0200

    added timer for data logging

--- src/main.c
+++ src/main.c
@@ -1,77 +1,90 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "esp_log.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "driver/adc.h"
-#include "esp_adc_cal.h"
+/*************************************************/
+/*<                  MCDEVICES                  >*/
+/*************************************************/
+/*<             DATALOGGER FOR PLANTS           >*/
+/*<                version: 1.0.1               >*/               
+/*************************************************/
 
+#include "main.h"
+#include "statistics.h"
 
-//ADC Channels
-#if CONFIG_IDF_TARGET_ESP32 //ESP32
-#define ADC1_EXAMPLE_CHAN0          ADC1_CHANNEL_6  //GPIO34
-#define ADC2_EXAMPLE_CHAN0          ADC2_CHANNEL_0 //GPIO4
-static const char *TAG_CH[2][10] = {{"ADC1_CH6"}, {"ADC2_CH0"}};
-#endif
+// ************************************************
+// **************** TIMER HANDLE ******************
+static TimerHandle_t scheduler_handle_id;
+static TimerHandle_t minutes_handle_id;
 
-//ADC Attenuation
-#define ADC_EXAMPLE_ATTEN           ADC_ATTEN_DB_11
+// ************************************************
+// ***************** VARIABLES ********************
+static uint16_t adc_raw;
+static uint16_t voltage;
+static uint16_t percentuale;
+static uint32_t num_samp = 0;
+static uint32_t minutesCounter = 0;
+unsigned long actual_timestamp = 0;
+Data_rec oneButterTx[10];
+esp_err_t ret = ESP_OK;
 
-//ADC Calibration
-#if CONFIG_IDF_TARGET_ESP32
-#define ADC_EXAMPLE_CALI_SCHEME     ESP_ADC_CAL_VAL_EFUSE_VREF
-#endif
-
-
-static int adc_raw[2][10];
-static int adc_raw1 = 0;
-static const char *TAG = "ADC SINGLE";
-
-//static esp_adc_cal_characteristics_t adc1_chars;
-static esp_adc_cal_characteristics_t adc2_chars;
-
-static bool adc_calibration_init(void)
-{
-    esp_err_t ret;
-    bool cali_enable = false;
-
-    ret = esp_adc_cal_check_efuse(ADC_EXAMPLE_CALI_SCHEME);
-    if (ret == ESP_ERR_NOT_SUPPORTED) {
-        ESP_LOGW(TAG, "Calibration scheme not supported, skip software calibration");
-    } else if (ret == ESP_ERR_INVALID_VERSION) {
-        ESP_LOGW(TAG, "eFuse not burnt, skip software calibration");
-    } else if (ret == ESP_OK) {
-        cali_enable = true;
-        esp_adc_cal_characterize(ADC_UNIT_2, ADC_EXAMPLE_ATTEN, ADC_WIDTH_BIT_DEFAULT, 0, &adc2_chars);
-    } else {
-        ESP_LOGE(TAG, "Invalid arg");
-    }
-
-    return cali_enable;
+void repeated_timer_handler_minute(TimerHandle_t xTimer){   //FOR DEBUGGING ONLY!!!
+    ++minutesCounter;
+    ESP_LOGI(TAG,"MAIN min counted \n\r");
+    actual_timestamp += 60;
+    ESP_LOGI(TAG,"MAIN Current time is %ld\n\r", actual_timestamp);
 }
 
-void app_main(void)
-{
-    esp_err_t ret = ESP_OK;
-    float voltage = 0.0;
+static void scheduler_thread(TimerHandle_t xTimer){
     bool cali_enable = adc_calibration_init();
-
     //ADC2 config
-    ESP_ERROR_CHECK(adc2_config_channel_atten(ADC2_EXAMPLE_CHAN0, ADC_EXAMPLE_ATTEN));
-
-    while (1) {
-    //while (ret == ESP_ERR_INVALID_STATE);
-        ret = adc2_get_raw(ADC2_EXAMPLE_CHAN0, ADC_WIDTH_BIT_DEFAULT, &adc_raw[1][0]);
-        ESP_ERROR_CHECK(ret);
-
-        ESP_LOGI(TAG_CH[1][0], "raw  data: %d", adc_raw[1][0]);
-        if (cali_enable) {
-            voltage = esp_adc_cal_raw_to_voltage(adc_raw[1][0], &adc2_chars);
-            if(adc_raw[1][0] == 0){
-                voltage = 0.0;
-            }
-            ESP_LOGI(TAG_CH[1][0], "cali data: %f mV", voltage);
+    gpio_set_level(PIN_LED,1);
+    ESP_ERROR_CHECK(adc2_config_channel_atten(ADC2_CHAN0, adc_atten));
+    ret = adc2_get_raw(ADC2_CHAN0, ADC_WIDTH_BIT_DEFAULT, &adc_raw); //read on single channel
+    ESP_ERROR_CHECK(ret);
+    if (cali_enable) {
+        voltage = esp_adc_cal_raw_to_voltage(adc_raw, &adc_chars);
+        ESP_LOGI(TAG,"raw  data: %d", adc_raw); //FOR DEBUGGING ONLY!!!
+        if(adc_raw == 0){
+            voltage = 0;
         }
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        ESP_LOGI(TAG, "voltage: %d mV", voltage); //FOR DEBUGGING ONLY!!!
+        percentuale = map(adc_raw,MIN_VALUE,MAX_VALUE,100,0);
+        ESP_LOGI(TAG, "%d", percentuale);   //prints the percentage of soil moisture
+    }
+    num_samp++;
+    oneButterTx[10].num_sample = num_samp;
+    ESP_LOGI(TAG, "Number of samples: %d ", oneButterTx[10].num_sample);
+    gpio_set_level(PIN_LED,0);
+}
+
+static void create_timers(void){
+    /*FREERTOS TIMER CREATE*/
+    scheduler_handle_id = xTimerCreate("SCHED",                  //Timer Name
+                                        pdMS_TO_TICKS(200),     //Period in ticks
+                                        pdTRUE,                 //Auto reload
+                                        NULL,                   //timer ID
+                                        &scheduler_thread);      //Callback Function
+    
+    minutes_handle_id   = xTimerCreate("SCHED",                  //Timer Name
+                                        pdMS_TO_TICKS(60000),     //Period in ticks
+                                        pdTRUE,                 //Auto reload
+                                        NULL,                   //timer ID
+                                        &repeated_timer_handler_minute);      //Callback Function
+
+    /* Error checking */
+    if ((NULL == scheduler_handle_id)
+        ||(NULL == minutes_handle_id))
+    {
+        ESP_LOGW(TAG, "ESP32_ERROR_MEM");
+    }
+
+    xTimerStart(scheduler_handle_id,0);
+    xTimerStart(minutes_handle_id,0);
+}
+
+void app_main(void){
+    //initialize.
+    pin_config();
+    create_timers();
+    //vTaskStartScheduler();
+    while (1) {
+        vTaskDelay(10);
     }
 }
