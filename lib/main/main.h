3d9cf8af4e3bcb799e084ac853433eb75dc49aaf
MCdevices <matteocavalera30@gmail.com>
2022-07-04 21:16:37 +0200

    added lib for wifi

--- lib/main/main.h
+++ lib/main/main.h
@@ -5,8 +5,8 @@
 // ******************* INCLUDES *******************
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
-#include "esp_log.h"
 
 //FreeRTOS
 #include "freertos/FreeRTOS.h"
@@ -14,22 +14,33 @@
 #include "freertos/event_groups.h"
 #include "freertos/timers.h"
 
+//Driver
 #include "driver/adc.h"
 #include "driver/gpio.h"
 #include "esp_adc_cal.h"
-#include <string.h>
+
+//Wifi - Time
 #include <time.h>
 #include <sys/time.h>
+#include "esp_wifi.h"
+#include "lwip/err.h"
+#include "lwip/apps/sntp.h"
+
+//ESP32
+#include "esp_log.h"
 #include "esp_system.h"
 #include "esp_event.h"
 #include "esp_attr.h"
 #include "esp_sleep.h"
 #include "nvs_flash.h"
 #include "esp_sntp.h"
+//#include "esp_event_loop.h"
+#include "esp_event.h"
 
 
 //MY INCLUDES
 #include "DataRec_config/DataRec_config.h"
+//#include "wifi_config_time.h"
 
 // ************************************************
 // ***************** DEFINITIONS ******************
@@ -43,9 +54,20 @@
 #define MIN_VALUE 910  //sensor in the water "extreme condition"
 #define NUM_MAX_SAMPLE 10
 
+//WIFI
+#define EXAMPLE_WIFI_SSID "Xperia XZ1_ad70"
+#define EXAMPLE_WIFI_PASS "matteocava"
+
+
+
 // ************************************************
 // ****************** VARIABLE ********************
 extern char *TAG;
 esp_adc_cal_characteristics_t adc_chars;
+EventGroupHandle_t wifi_event_group;
+time_t now;
+struct tm timeinfo;
+
+
 
 #endif
