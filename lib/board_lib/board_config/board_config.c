9f94751d02c415633f8167031e19929051fe53ca
MCdevices <matteocavalera30@gmail.com>
2022-07-10 20:45:03 +0200

    changed func for ADC

--- lib/board_lib/board_config/board_config.c
+++ lib/board_lib/board_config/board_config.c
@@ -4,36 +4,32 @@ char *TAG = "ADC READ";
 
 void pin_config(void){
     gpio_reset_pin(LED_PIN);    //GPIO2
-    //gpio_reset_pin(RELAY_PIN);  //GPIO5
+    gpio_reset_pin(RELAY_PIN);  //GPIO5
     gpio_set_direction(LED_PIN,GPIO_MODE_OUTPUT);
-    //gpio_set_direction(RELAY_PIN,GPIO_MODE_OUTPUT);
-    //gpio_set_level(RELAY_PIN,0);
+    gpio_set_direction(RELAY_PIN,GPIO_MODE_OUTPUT);
 }
 
-
-bool adc_calibration_init(void){
-    esp_err_t ret;
-    bool cali_enable = false;
-
-    ret = esp_adc_cal_check_efuse(ADC_EXAMPLE_CALI_SCHEME);
-    if (ret == ESP_ERR_NOT_SUPPORTED) {
-        ESP_LOGW(TAG, "Calibration scheme not supported, skip software calibration");
-    } else if (ret == ESP_ERR_INVALID_VERSION) {
-        ESP_LOGW(TAG, "eFuse not burnt, skip software calibration");
-    } else if (ret == ESP_OK) {
-        cali_enable = true;
-        esp_adc_cal_characterize(ADC_UNIT_2, adc_atten, ADC_WIDTH_BIT_DEFAULT, 0, &adc_chars);
+void check_efuse(void){
+    //Check if TP is burned into eFuse
+    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_TP) == ESP_OK) {
+        printf("eFuse Two Point: Supported\n");
     } else {
-        ESP_LOGE(TAG, "Invalid arg");
+        printf("eFuse Two Point: NOT supported\n");
     }
-    return cali_enable;
+    //Check Vref is burned into eFuse
+    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_VREF) == ESP_OK) {
+        printf("eFuse Vref: Supported\n");
+    } else {
+        printf("eFuse Vref: NOT supported\n");
+    }
+
 }
 
 int32_t map(uint16_t x, uint16_t in_min, uint16_t in_max, uint16_t out_min, uint16_t out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-//SLEEP MODE
+//SLEEP MODE IN SEC
 void deep_sleep_mode(uint8_t time_sleep_s){
     ESP_LOGI(TAG, "Entering deep sleep for %d seconds", time_sleep_s);
     esp_wifi_stop(); //turn off wifi 
