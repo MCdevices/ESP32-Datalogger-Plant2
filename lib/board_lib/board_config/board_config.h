5d1a5e929864512e2a5ab87b7618c69625f24d29
MCdevices <matteocavalera30@gmail.com>
2022-07-10 20:45:33 +0200

    added check_efuse

--- lib/board_lib/board_config/board_config.h
+++ lib/board_lib/board_config/board_config.h
@@ -3,8 +3,8 @@
 
 #include "main.h"
 
-bool adc_calibration_init(void);
 void pin_config(void);
+void check_efuse(void);
 int32_t map(uint16_t x, uint16_t in_min, uint16_t in_max, uint16_t out_min, uint16_t out_max);
 void deep_sleep_mode(uint8_t time_sleep_s);
 
