530060ceba52638642ee2eac5564ba8c622e6767
MCdevices <matteocavalera30@gmail.com>
2022-07-15 23:53:44 +0200

    added sd card read write

--- lib/board_lib/board_config/board_config.h
+++ lib/board_lib/board_config/board_config.h
@@ -5,6 +5,8 @@
 
 void pin_config(void);
 void check_efuse(void);
+void read_sd_card(char *file_data_read, FILE *file_open);
+void write_sd_card(sdmmc_card_t *card, FILE *file_open, char *file_data_write, char buf[], char mount_point[]);
 int32_t map(uint16_t x, uint16_t in_min, uint16_t in_max, uint16_t out_min, uint16_t out_max);
 void deep_sleep_mode(uint8_t time_sleep_s);
 
