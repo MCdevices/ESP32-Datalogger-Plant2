3e61be6e5298192ad02737e96adc5ed7b7c333ca
MCdevices <matteocavalera30@gmail.com>
2022-07-15 23:53:14 +0200

    added sdcard init

--- lib/board_lib/board_config/board_config.c
+++ lib/board_lib/board_config/board_config.c
@@ -1,6 +1,7 @@
 #include "board_config/board_config.h"
 
 char *TAG = "ADC READ";
+char line[64];
 
 void pin_config(void){
     gpio_reset_pin(LED_PIN);    //GPIO2
@@ -27,6 +28,44 @@ void check_efuse(void){
 
 int32_t map(uint16_t x, uint16_t in_min, uint16_t in_max, uint16_t out_min, uint16_t out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+void write_sd_card(sdmmc_card_t *card, FILE *file_open, char *file_data_write, char buf[], char mount_point[]){
+    // Card has been initialized, print its properties
+    sdmmc_card_print_info(stdout, card);
+    ESP_LOGI(TAG, "Opening file %s", file_data_write);
+    file_open = fopen(file_data_write, "w");
+    if (file_open == NULL) {
+        ESP_LOGE(TAG, "Failed to open file for writing");
+        return;
+    }
+    fprintf(file_open, "AUTOMATED IRRIGATION!\nDate/time in Italy is: %s \n",buf); //card->cid.name,
+    fclose(file_open);
+    ESP_LOGI(TAG, "File written");
+
+    // All done, unmount partition and disable SPI peripheral
+    esp_vfs_fat_sdcard_unmount(mount_point, card);
+    ESP_LOGI(TAG, "Card unmounted");
+}
+
+void read_sd_card(char *file_data_read, FILE *file_open){
+    // Open file for reading
+    ESP_LOGI(TAG, "Reading file %s", file_data_read);
+    file_open = fopen(file_data_read, "r");
+    if (file_open == NULL) {
+        ESP_LOGE(TAG, "Failed to open file for reading");
+        return;
+    }
+
+    fgets(line, sizeof(line), file_open);
+    fclose(file_open);
+
+    // Strip newline
+    char *pos = strchr(line, '\n');
+    if (pos) {
+        *pos = '\0';
+    }
+    ESP_LOGI(TAG, "Read from file: '%s'", line);
 }
 
 //SLEEP MODE IN SEC
