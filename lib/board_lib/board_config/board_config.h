2a677a0e9cf2bcc3f7fcc8b8503703546e064bad
MCdevices <matteocavalera30@gmail.com>
2022-07-27 20:53:12 +0200

    improved the writing system on micro SD

--- lib/board_lib/board_config/board_config.h
+++ lib/board_lib/board_config/board_config.h
@@ -6,8 +6,7 @@
 void pin_config(void);
 void check_efuse(void);
 void read_sd_card(char *file_data_read, FILE *file_open);
-void write_sd_card(sdmmc_card_t *card, FILE *file_open, char *file_data_write, char buf[],char mount_point[]);
-void append_data_sd_card(sdmmc_card_t *card, FILE *file_open, char *file_data_write,uint16_t value ,char mount_point[]);
+void write_sd_card(FILE *file_to_open, char *file_name ,char mount_point[], char bufTime[], int32_t perc, uint32_t num_samp);
 void spi_disable(sdmmc_card_t *card,char mount_point[]);
 int32_t map(uint16_t x, uint16_t in_min, uint16_t in_max, uint16_t out_min, uint16_t out_max);
 void deep_sleep_mode(uint8_t time_sleep_s);
